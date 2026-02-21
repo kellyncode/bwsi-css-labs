f61fd5af9213f17f75230662b3c96f0030be8b03
kellyncode <ablukelly@gmail.com>
2026-02-21 14:57:06 -0500

    Update lab_1b.py
    sanitized numbers

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -9,7 +9,7 @@ and prints the result to the terminal window.
 
 """
 
-def simple_calculator(operation: str, num1: float, num2: float) -> float:
+def request_sanitized_number(prompt:str) -> float:
     """
     Function that takes in two numbers and an operation (add, subtract, multiply, divide),
     then performs the operation on the two numbers and returns the result.
@@ -27,30 +27,18 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
             number = float(input(prompt))
             return number
         except ValueError:
-            print("invalid input. Please enter a valid number.")
-
-    if operation == "add":
-        return num1 + num2
-    elif operation == "subtract":
-        return num1 - num2
-    elif operation == "multiply":
-        return num1 * num2
-    elif operation == "divide":
-        if num2 != 0:
-            return num1 / num2
-        else:
-            raise ValueError("Cannot divide by zero.")
-    else:
-        raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
-
+            print("Invalid input. Please enter a valid number.")
+ 
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = request_sanitized_number("Enter the first number: ")
+    num2 = request_sanitized_number("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+
 
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
